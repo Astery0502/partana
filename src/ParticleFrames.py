fbaa1062a9f77596f0c2a0a971d27ed72ff8dc9b
Astty <hwu@smail.nju.edu.cn>
2024-06-03 03:29:32 +0800

    modify a little bug“

--- src/ParticleFrames.py
+++ src/ParticleFrames.py
@@ -4,11 +4,8 @@ import re
 import numpy as np
 import pandas as pd 
 
-import matplotlib.pyplot as plt 
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import plotly.express as px
-from mpl_toolkits.mplot3d import Axes3D
 
 from typing import Union, List, Tuple, Callable, Iterable
 
